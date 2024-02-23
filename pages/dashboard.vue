83226193aa0a3aa25c4617c9abc85279be9b829f
kreker2112 <kreker2112@gmail.com>
2024-02-24 01:51:13 +0200

    some changes

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,7 +3,7 @@
     class="flex py-60 flex-col h-screen my-auto items-center bgimg bg-cover bg-gradient-to-b from-white to-blue-200 dark:bg-gradient-to-b dark:from-slate-800 dark:to-slate-400"
   >
     <div
-      class="mt-2 bg-white font-bold text-7xl py-1 px-8 rounded m-2 dark:bg-slate-800 dark:text-white"
+      class="mt-2 bg-white font-bold text-7xl py-1 px-8 rounded m-2 dark:bg-slate-700 dark:text-white"
     >
       Welcome to the dashboard <span v-if="user.name"> {{ user.name }}</span>
     </div>
